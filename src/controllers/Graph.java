5e5b68593b4ce1462ecd27b8bef5bc88f328ead7
Sebastian <derlis567y@gmail.com>
2025-07-10 08:59:27 -0500

    Terminada

--- src/controllers/Graph.java
+++ src/controllers/Graph.java
@@ -1,44 +1,74 @@
-package controllers;
+    package controllers;
 
-import java.util.List;
-import models.Node;
+    import java.util.HashSet;
+    import java.util.List;
+    import java.util.Set;
+    import models.Node;
 
-public class Graph {
-    private List<Node> nodes;  
+    public class Graph {
+        private Set<Node> nodes;  
 
-    public Graph(){
+        public Graph(){
+            nodes = new HashSet<>();
+        }
 
+        // Agregar un nodo
+        public Node addNode(int value){
+            Node node = new Node(value);
+            nodes.add(node);
+
+            return node;
+        }
+
+        // Metodo para agregar una arista
+        public void addEdge(Node src, Node dest){
+            src.addNeighbor(dest);
+            dest.addNeighbor(src);
+        }
+
+        public void addEdgeUni(Node scr, Node dest){
+            scr.addNeighbor(dest);
+        }
+
+        // Metodo para impromir el grafo
+        // Para cada nodo imprime sus conexiones.
+        public void printGraph() {
+            for (Node node : nodes) {
+                System.out.print("Vertex " + node.getValue() + ": ");
+                List<Node> vecinos = node.getNeighbors();
+                
+                vecinos.sort((n1, n2) -> Integer.compare(n1.getValue(), n2.getValue()));
+
+                if (vecinos != null && !vecinos.isEmpty()) {
+                    for (int i = 0; i < vecinos.size(); i++) {
+                        System.out.print(vecinos.get(i).getValue());
+                        if (i < vecinos.size() - 1) { 
+                            System.out.print(" -> ");
+                        }
+                    }
+                } 
+                System.out.println();
+            }
+        }
+
+        // Metodo DFS
+        public void getDFS(Node startNode){
+
+        }
+
+        private void getDFSUtil(Node node, Boolean[] visited){
+
+        }
+
+        public void getBFS(Node startNode){
+
+        }
+
+        public int[][] getAdjacencyMatrix(){
+            return null;
+        }
+
+        public void printAdjacencyMatrix(){
+
+        }
     }
-
-    public Node addNode(int value){
-        return Node;
-    }
-
-    public void addEdge(Node src, Node dest){
-
-    }
-
-    public void printGraph(){
-
-    }
-
-    public void getDFS(Node startNode){
-
-    }
-
-    private void getDFSUtil(Node node, Boolean[] visited){
-
-    }
-
-    public void getBFS(Node startNode){
-
-    }
-
-    public int[][] getAdjacencyMatrix(){
-        return null;
-    }
-
-    public void printAdjacencyMatrix(){
-
-    }
-}
--- src/models/Node.java
+++ src/models/Node.java
@@ -1,5 +1,41 @@
 package models;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Node {
+    private int value;
+    private List<Node> neighbors;
+
+    public Node(int value){
+        this.value = value;
+        this.neighbors = new ArrayList<>(); 
+    }
+
+    public int getValue(){
+        return value; 
+    }
+
+    public List<Node> getNeighbors(){
+        return neighbors;
+    }
+
+    public void addNeighbor(Node neighbor){
+        if (!this.neighbors.contains(neighbor)) {
+            this.neighbors.add(neighbor);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return value == node.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(value);
+    }
 }
