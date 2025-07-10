838f169b173a78a1b203de2b3730d10c5dc09036
Sebastian <derlis567y@gmail.com>
2025-07-10 08:26:31 -0500

    first commit

---
+++ src/controllers/Graph.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.util.List;
+import models.Node;
+
+public class Graph {
+    private List<Node> nodes;  
+
+    public Graph(){
+
+    }
+
+    public Node addNode(int value){
+        return Node;
+    }
+
+    public void addEdge(Node src, Node dest){
+
+    }
+
+    public void printGraph(){
+
+    }
+
+    public void getDFS(Node startNode){
+
+    }
+
+    private void getDFSUtil(Node node, Boolean[] visited){
+
+    }
+
+    public void getBFS(Node startNode){
+
+    }
+
+    public int[][] getAdjacencyMatrix(){
+        return null;
+    }
+
+    public void printAdjacencyMatrix(){
+
+    }
+}
